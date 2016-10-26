8b2407441009ede1ff9491b13b445a8866b44700
Reinis Riekstins <riekstins.reinis@gmail.com>
2016-10-26 10:09:08 +0300

    added Foundation accordion

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import c3 from 'c3'
 import _ from 'lodash'
-import $ from 'jquery'
+//import $ from 'jquery'
 //import React from 'react'
 //import ReactDOM from 'react-dom'
 
+
 $(() => {
-  $('#wrapper').html('Everything working OK.')
+  let accordion = new Foundation.Accordion($('.accordion'))
+
+  $('#accordion-1').html('Everything working OK.')
 })
