96ec7e3397b1980ac5ee4a2ddfaf65699906da86
Reinis Riekstins <riekstins.reinis@gmail.com>
2016-10-26 16:48:30 +0300

    started laying out async control flow logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,89 @@
 import axios from 'axios'
 import c3 from 'c3'
 import _ from 'lodash'
-//import $ from 'jquery'
-//import React from 'react'
-//import ReactDOM from 'react-dom'
+import url from 'url'
+
+const validate = {
+  logHtml: () => {
+
+  },
+  logJson: () => {
+
+  }
+}
+
 
 
 $(() => {
   let accordion = new Foundation.Accordion($('.accordion'))
 
-  $('#accordion-1').html('Everything working OK.')
+  // create array of promises
+  let promises = new Array(
+    document.querySelectorAll('.log-input').length
+  )
+
+  // on input value change validate url and
+  // send request with axios
+  $('.log-input').each((i, elem) => {
+    $(elem).on('keyup', _.debounce(() => {
+
+      let reqUrl = $(elem).val()
+
+      // for url module to be ablt to parse reqUrl
+      reqUrl.startsWith('http://') ? null : reqUrl = 'http://' + reqUrl
+
+      let parsedUrl = url.parse(reqUrl),
+          hostUrl = parsedUrl.host,
+          urlPath = parsedUrl.path
+      
+      function validLogsUrl() {
+        return (hostUrl === 'logs.tf' && urlPath.match(/^\/\d{5,}$/g))
+      }
+      /*
+      if ( validLogsUrl() ) {
+        axios.all([
+          axios(`http://logs.tf/json${urlPath}`),
+          axios(`http://logs.tf${urlPath}`)
+        ]).then((res) => {
+          let data = res.data
+
+        })//.catch((err) => console.log(err))
+      }
+      else {
+
+      }*/
+      /// if valid, send axios requests
+      /// .then() validate received data
+      /// if not valid, display message
+      /// otherwise save to the promises array
+    }, 300))
+  })
+
+  $('#generate-1').click(() => {
+    /// take the array of promises, flatten it(?)
+    /// and Promise.all() them
+
+    /// generate list of players in accordion-2
+  })
+
+  // when user click on button generate
+  // it unlock and opens next accordion pane
+  $('[id^=generate-]').each((i, elem) => {
+    $(elem).click(() => {
+      $(`#accordion-${i+2}`).removeClass('disabled')
+      $('.accordion').foundation('down', $(`#accordion-${i+2} .accordion-content`))
+    })
+  })
+
+  //$('#accordion-1').html(inputs)
+
+  //$('input[type=text]:nth(0)').focus()
+  
+  // on change, move cursor to next input
+  //$('input[type=text]').each((i, elem) => {
+  //  $(`input[type=text]:nth(${i})`).on('change paste keyup', 
+  //    () => $(`input[type=text]:nth(${i+1})`).focus().select()
+  //  )
+  //})
+  
 })
