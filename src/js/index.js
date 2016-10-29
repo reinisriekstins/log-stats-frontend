df0a6c42ac7946fd66c575d59eb26d9155d828be
Reinis Riekstins <rr14022@lu.lv>
2016-10-29 21:15:23 +0300

    put some code into separate functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
+'use strict';
 import axios from 'axios'
 import c3 from 'c3'
 import _ from 'lodash'
 import url from 'url'
 
-function validateInputVal (inputVal) {
+const validateInputVal = inputVal => {
   if ( !inputVal || /\s+/g.test( inputVal ) ) return null
 
   let parsedUrl, hostUrl, urlPath
@@ -27,6 +28,61 @@ function validateInputVal (inputVal) {
   else return false
 }
 
+const tryParseJson = (json, reviver) => {
+    try {
+        return JSON.parse(json, reviver)
+    } catch(error){
+        console.log(error)
+    }
+}
+
+const getJson = (urlPath, depth) => {
+  depth = depth || 1
+  axios
+    .get(`http://logs.tf/json${urlPath}`)
+    .catch(console.log)
+    .then(res => {
+      if ( tryParseJson(res.data) ) return res.data
+      else if ( $(res.data).find('.log-section > h3').html() === 'Something went wrong' ) {
+        setTimeout(depth => {
+          if (depth !== 20) {
+            depth++
+            getJson(urlPath, depth)
+          }
+        }, 10)
+      }
+      else throw new Error('something went horribly wrong')
+    })
+    .catch(console.log)
+}
+
+const dryGet = (urlPath, depth) => {
+  depth = depth || 1
+  //let returnVal = new Promise()
+  return axios
+    .get(`http://logs.tf${urlPath}`)
+    .then(res => {
+      if ( tryParseJson(res.data) ) return res.data
+      else if ( $(res.data).find('.log-section > h3').html() === 'Something went wrong' ) {
+        return new Promise ((resolve, reject) => {
+          setTimeout(depth => {
+            if (depth <= 20) getJson(urlPath, ++depth).then(resolve)
+            else reject('recursion is going deep')
+          }, 10)
+        })
+      }
+      else if ( $(res.data).length ) {
+        let map = $(res.data).find('#log-map').html()
+        let time = $(res.data).find('.datefield').attr('data-timestamp') + '000'
+
+        return { urlPath, time, map }
+      }
+      else throw new Error('something went horribly, horribly wrong')
+    })
+    //.catch(console.log)
+
+}
+
 $(() => {
   let accordion = new Foundation.Accordion($('.accordion'))
 
@@ -35,25 +91,23 @@ $(() => {
     document.querySelectorAll('.log-input').length*2
   )
 
-  
-
   // on input value change validate url and
   // send request with axios
   $('.log-input').each((i, elem) => {
     $(elem).on('keyup', _.debounce(() => {
 
-      let url = validateInputVal( $(elem).val() )
+      let urlPath = validateInputVal( $(elem).val() )
 
-      if (url) {
+      if (urlPath) {
         //// update dom to remove notification
         // axios
       }
       else //// update dom with notification
-      
+
       Promise
         .resolve( )
         .then(axios.get)// http request via axios
-      
+
       if ( validLogsUrl() ) {
           axios(`http://logs.tf/json${urlPath}`).then()//validate recursively then push into promises arr),
           axios(`http://logs.tf${urlPath}`)// see above
@@ -69,7 +123,7 @@ $(() => {
   })
 
   $('#generate-1').click(() => {
-    /// Promise.all(promises) 
+    /// Promise.all(promises)
     /// if all resolutions are null, dont do shit
 
     /// generate list of players in accordion-2
@@ -87,12 +141,12 @@ $(() => {
   //$('#accordion-1').html(inputs)
 
   //$('input[type=text]:nth(0)').focus()
-  
+
   // on change, move cursor to next input
   //$('input[type=text]').each((i, elem) => {
-  //  $(`input[type=text]:nth(${i})`).on('change paste keyup', 
+  //  $(`input[type=text]:nth(${i})`).on('change paste keyup',
   //    () => $(`input[type=text]:nth(${i+1})`).focus().select()
   //  )
   //})
-  
+
 })
